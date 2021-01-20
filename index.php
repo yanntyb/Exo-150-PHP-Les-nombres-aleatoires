efc15a5a1e2c15c2a7143089e36a9dfb7776030b
Nux007 <007.nux@gmail.com>
2021-01-20 07:37:19 +0100

    Exo 150.

---
+++ index.php
@@ -0,0 +1,40 @@
+<?php
+
+/**
+ * 1. Générez un nombre aléatoire compris entre 0 et 50, affichez le avec echo dans un nouveau <div>.
+ */
+
+// TODO Votre code ici.
+
+
+
+/**
+ * 2. Générez un nombre aléatoire compris entre 50 et getrandmax, affichez le résultat dans un nouveau <p>
+ */
+
+// TODO Votre code ici.
+
+
+/**
+ * 3. Générez un nombre aléatoire compris entre 0 et 50
+ * ==> si le nombre est inférieur ou égal à 50, affichez : vous avez gagné ( dans un nouveau <p> )
+ * ==> si ce n'est pas le cas, affichez : vous avez perdu ( dans un nouveau <p> )
+ */
+
+// TODO Votre code ici.
+
+
+/**
+ * 4. Créez une fonction qui génère un nombre aléatoire compris entre $max et $min ( paramètres ).
+ * ==> Si le nombre est compris entre $max et $max -100, regénérez un nouveau nombre avec un appel récursif.
+ * ==> Si ce n'est pas le cas, retournez tout simplement le nombre généré.
+ */
+
+// TODO Votre code ici.
+
+
+
+
+
+
+
